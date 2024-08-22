07be12d3e77139f0d33e91d8f9238e186ed3f31a
federodriguez33 <federicosebastianrodriguez@gmail.com>
2024-08-21 21:10:28 -0300

    buscador

--- GymApp/src/sections/Rutines/RutinesView.jsx
+++ GymApp/src/sections/Rutines/RutinesView.jsx
@@ -11,6 +11,13 @@ const RutinesView = () => {
     const [formValues, setFormValues] = useState(state.rutine);
     const [isEditing, setIsEditing] = useState(false);
 
+    // Buscador
+    const [searchRutine, setSearchRutine] = useState("");
+
+    const filteredRutine = formValues.exercices.filter((exercise) =>
+        exercise.name.toLowerCase().includes(searchRutine.toLowerCase())
+    );
+
     const handleChange = (e) => {
         const { name, value } = e.target;
         setFormValues({
@@ -112,6 +119,28 @@ const RutinesView = () => {
                 ) : (
                     <p className="mb-4">{rutine.difficulty}</p>
                 )}
+
+                {/* BUSCADOR */}
+                <div className="mb-4">
+                    <input
+                        type="text"
+                        placeholder="Buscar ejercicio por nombre"
+                        className="p-2 w-full border-2 border-gray-300 rounded"
+                        value={searchRutine}
+                        onChange={(e) => setSearchRutine(e.target.value)}
+                    />
+                </div>
+
+                <div className='flex flex-wrap gap-8 justify-center'>
+                    {filteredRutine.map((exercise) => (
+                        <ExerciseCard
+                            key={exercise.id}
+                            exercise={exercise}
+                            isEditing={isEditing}
+                            onEdit={(updatedExercise) => handleEditExercise(exercise.id, updatedExercise)}
+                        />
+                    ))}
+                </div>
 
                 <p className="font-semibold">Ejercicios:</p>
                 <div className='flex flex-wrap gap-8 justify-center'>
